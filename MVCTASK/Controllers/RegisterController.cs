1025ff4ceb17ace0e10cdfebcdc3e6b0eeb2e5cf
atheeradel <atheeradel4628@yahoo.com>
2024-04-27 21:53:04 +0300

    Add project files.

---
+++ MVCTASK/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTASK.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
